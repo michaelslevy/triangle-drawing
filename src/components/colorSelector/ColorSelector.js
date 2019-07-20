1487f1a6c6aaf1fa89a29c0ad3534c744ff73fe5
Michael Levy <michael.levy.vt@gail.com>
2019-07-19 22:46:49 -0400

    FEAT: seperate color search into its own component

--- src/components/colorSelector/ColorSelector.js
+++ src/components/colorSelector/ColorSelector.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types';
 import { connect } from 'react-redux'
 import SelectorBlock from "./SelectorBlock"
 import './index.css'
+import ColorSearch from "./ColorSearch"
 
 //function passed to Reduxes Connect to populate store
 const mapStateToProps = (store) => {
@@ -20,62 +21,6 @@ const mapDispatchToProps = (dispatch) => {
 
 class ColorSelector extends Component {
 
-    constructor(){
-      super();
-      this.state = {
-        palettes:[],
-        loading:true
-      };
-
-      this.inputRef = React.createRef();
-      this.updateKeyword = this.updateKeyword.bind(this);
-
-    }
-
-    parseColorJson=function(json){
-      let data=[];
-      for (let i=0; i<json.length; i++){
-
-        let colors=json[i].colors;
-        let colorList=[];
-        for(let x=0; x<colors.length; x++){
-          colorList.push(colors[x]);
-        }
-
-        let obj={title:json[i].title, colors:colorList}
-        data.push(obj);
-      }
-      return data;
-    }
-
-    getData(keywords=false){
-      const self=this;
-      const url='http://webventions.com/sandbox/fetch/fetch.php';
-      let fullLink=(keywords)?url+"?keywords="+encodeURI(keywords):url;
-      self.setState({loading:true});
-      fetch(fullLink).then(function(response) {
-        return response.json();
-      }).then(function(json) {
-        let p=self.parseColorJson(json);
-        self.setState({palettes:p})
-        self.setState({loading:false});
-      }).catch(function(err) {
-        console.error('Fetch problem: ' + err.message);
-      });
-  }
-
-  updateKeyword=function(event){
-    event.preventDefault();
-    event.stopPropagation();
-
-    let keywords=this.inputRef.current.value;
-    this.getData(keywords);
-  }
-
-  componentDidMount(){
-    this.getData();
-  }
-
    render() {
 
      return (
@@ -86,19 +31,7 @@ class ColorSelector extends Component {
             <button>Builder</button>
           </nav>
 
-          <form>
-            <input placeholder='enter keywords' type='text'ref={this.inputRef} id='colorSearch' />
-            <button id='colorSubmit' onClick={this.updateKeyword} >Explore</button>
-          </form>
-
-          <div id='colorList'>
-
-            {(this.state.loading)?<p>loading...</p>:
-            this.state.palettes.map((palette, index) =>(
-              <SelectorBlock key={index} title={palette.title}  colors={palette.colors} />
-            ))}
-
-          </div>
+          <ColorSearch />
        </div>
      );
   }
